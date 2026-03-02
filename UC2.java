d569af9262252c589511c6c69193ed8090adea25
as <as5819@srmist.edu.in>
2026-03-02 23:04:27 +0500

    dev/UC-2

---
+++ UC2.java
@@ -0,0 +1,12 @@
+public class UC2 {
+    public static void print() {
+
+        System.out.println("  *****  " + "  *****  " + "  ****** " + "  ****** ");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *     *");
+        System.out.println(" *     * " + " *     * " + " *     * " + " *      ");
+        System.out.println(" *     * " + " *     * " + "  ****** " + "  ***** ");
+        System.out.println(" *     * " + " *     * " + " *       " + "       *");
+        System.out.println(" *     * " + " *     * " + " *       " + " *     *");
+        System.out.println("  *****  " + "  *****  " + " *       " + "  ***** ");
+    }
+}
