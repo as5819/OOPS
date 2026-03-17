b0f7b520e9feb7a41a76bfceddb9b073f35914fd
Aditya Sharma <as5819@srmist.edu.in>
2026-03-17 09:14:34 +0500

    feat: UC4 - display multiple names

---
+++ UC4_2.java
@@ -0,0 +1,13 @@
+import java.util.Collections;
+
+public class UC4_2 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello World");
+        } else {
+            for (String name : args) {
+                System.out.println("Hello " + name);
+            }
+        }
+    }
+}
