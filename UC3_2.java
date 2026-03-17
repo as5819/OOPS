621c67ab3f366dc880a60ad1be1d8be7038a6569
Aditya Sharma <as5819@srmist.edu.in>
2026-03-17 09:14:31 +0500

    feat: UC3 - display name with default

---
+++ UC3_2.java
@@ -0,0 +1,8 @@
+import java.util.Collections;
+
+public class UC3_2 {
+    public static void main(String[] args) {
+        String name = args.length > 0 ? args[0] : "World";
+        System.out.println("Hello " + name);
+    }
+}
